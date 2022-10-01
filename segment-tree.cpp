bed0482fb6cb4e1b9e54aac71c55daf2ff5c52ca
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2022-10-01 11:46:01 +0530

    Update segment-tree.cpp

--- segment-tree.cpp
+++ segment-tree.cpp
@@ -1,119 +1,15 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long int
-#define for0(i, n) for (int i = 0; i < n; i++)
-#define vi vector<int>
-#define pb push_back
-#define sz(x) sizeof(x)
-#define vec(n, x) vector<int> v(n, x)
-const ll INF = 1e18;
-const ll MOD = 1000000007;
-#define hi        \
-    ll tests;     \
-    cin >> tests; \
-    while (tests--)
-#define all(x) x.begin(), x.end()
-#define readv(vec)      \
-    for (auto &x : vec) \
-    {                   \
-        cin >> x;       \
-    }
-#define PHI M_PI
-#define graph                   \
-    int V;                      \
-    cin >> V;                   \
-    vector<int> adj[V];         \
-    int e;                      \
-    for (int i = 0; i < e; i++) \
-    {                           \
-        int u, v;               \
-        cin >> u >> v;          \
-        adj[u].push_back(v);    \
-        adj[v].push_back(u);    \
-    }
-#define printv(vec)       \
-    for (auto x : vec)    \
-    {                     \
-        cout << x << ' '; \
-    }                     \
-    cout << endl;
-#define sortasc(vec) sort(all(vec));
-#define sortdes(vec) sort(vec.rbegin(), vec.rend());
-#define setpre(x) cout << fixed << setprecision(10) << x << endl
-#define kick cout << 'C' << 'a' << 's' << 'e' << ' ' << '#' << k << ':' << ' '
-vector<ll> sieve(int n)
-{
-    int *tree = new int[n + 1]();
-    vector<ll> vect;
-    for (int i = 2; i <= n; i++)
-        if (tree[i] == 0)
-        {
-            vect.push_back(i);
-            for (int j = 2 * i; j <= n; j += i)
-                tree[j] = 1;
-        }
-    return vect;
-}
-bool pal(string s)
-{
-    int i = 0;
-    int j = s.size() - 1;
-    while (i < j)
-    {
-        if (s[i] != s[j])
-        {
-            return false;
-        }
-        i++;
-        j--;
-    }
-    return true;
-}
-ll op1(ll a, ll b, ll m)
-{
-    a = a % m;
-    b = b % m;
-    return (((a + b) % m) + m) % m;
-}
-ll op2(ll a, ll b, ll m)
-{
-    a = a % m;
-    b = b % m;
-    return (((a * b) % m) + m) % m;
-}
-ll op3(ll a, ll b, ll m)
-{
-    a = a % m;
-    b = b % m;
-    return (((a - b) % m) + m) % m;
-}
-int powerexpo(int x, int y, int p)
-{
-    int res = 1;
-    x = x % p;
-    if (x == 0)
-        return 0;
-    while (y > 0)
-    {
-        if (y & 1)
-        {
-            res = (res * x) % p;
-        }
-        y = y >> 1;
-        x = (x * x) % p;
-    }
-    return res;
-}
-ll C(ll a, ll b) { return (a + b - 1) / b; }
-// code by CasioFx-69
 struct Segtree
 {
     vector<int> v;
+    vector<int>tree;
     Segtree(vector<int> arr)
     {
         v = arr;
+        int n=v.size();
+        vector<int> t(4 * n);
+        tree = t;
     }
-    void build(int node, int start, int end, vector<int> &tree)
+    void build(int node, int start, int end)
     {
         //start with node 1 ->> call for  start and end.
         // if start and end are equal then we have a single node;
@@ -127,11 +23,11 @@ struct Segtree
         int mid = (start + end) / 2;
         int left = 2 * node;
         int right = 2 * node + 1;
-        build(left, start, mid, tree);
-        build(right, mid + 1, end, tree);
+        build(left, start, mid);
+        build(right, mid + 1, end);
         tree[node] = max(tree[left],tree[right]);
     }
-    int query(vector<int> &tree, int node, int l, int r, int start, int end)
+    int query(int node, int l, int r, int start, int end)
     {
         // if end < startidx of query or start>endidx of query
         //return -1;
@@ -156,8 +52,8 @@ struct Segtree
             //call for start->mid with node->number 2*node;
             // mid+1->end; with node number -> 2*node+1;
             int mid = (start + end) / 2;
-            int left = query(tree, node * 2, l, r, start, mid);
-            int right = query(tree, node * 2 + 1, l, r, mid + 1, end);
+            int left = query(node * 2, l, r, start, mid);
+            int right = query(node * 2 + 1, l, r, mid + 1, end);
             return max(left,right); // maxof(left,right);
         }
     }
@@ -169,9 +65,9 @@ int main()
     cin >> n;
     vector<int> v(n);
     readv(v); // input;
-    vector<int> tree(4 * n);
+    
     Segtree se = Segtree(v);
-    se.build(1,0,n-1,tree);
+    se.build(1,0,n-1);
     int k;
     cin >> k;
     while (k--)
@@ -180,7 +76,7 @@ int main()
         cin >> l >> r;
         l--;
         r--;
-        cout << se.query(tree, 1, l, r, 0, n - 1) << endl;
+        cout << se.query(1, l, r, 0, n - 1) << endl;
     }
 
     return 0;
